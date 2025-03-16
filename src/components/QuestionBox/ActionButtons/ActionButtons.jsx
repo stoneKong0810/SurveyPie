0eee698f656b1b861cd12aafd4bdee71d4910dfa
stoneKong0810 <stonekong2115@gmail.com>
2025-03-16 16:27:46 +0900

    Feat: 라우팅 네비게이션 추가

--- src/components/QuestionBox/ActionButtons/ActionButtons.jsx
+++ src/components/QuestionBox/ActionButtons/ActionButtons.jsx
@@ -1,10 +1,38 @@
+import { useNavigate } from 'react-router';
+
 function ActionButtons({ steps, questionsLength }) {
   const isLast = questionsLength - 1 === steps;
 
+  const navigate = useNavigate();
+
   return (
     <div>
-      {steps === 0 || <button>이전</button>}
-      {isLast ? <button>제출</button> : <button>다음</button>}
+      {steps === 0 || (
+        <button
+          onClick={() => {
+            navigate(`${steps - 1}`);
+          }}
+        >
+          이전
+        </button>
+      )}
+      {isLast ? (
+        <button
+          onClick={() => {
+            navigate('/done');
+          }}
+        >
+          제출
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            navigate(`${steps + 1}`);
+          }}
+        >
+          다음
+        </button>
+      )}
     </div>
   );
 }
