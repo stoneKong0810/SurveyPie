12a2e1fe09036d8d7cf83a62179455adafed2929
stoneKong0810 <stonekong2115@gmail.com>
2025-03-16 16:26:57 +0900

    Feat: 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import { useState } from 'react';
+import { Route, Routes } from 'react-router';
 
-import ProgressIndicator from './components/ProgressIndicator/ProgressIndicator';
-import QuestionBox from './components/QuestionBox/QuestionBox';
-import questionsData from './Data/questionsData';
+import CompletionPage from './pages/CompletionPage/CompletionPage';
+import SurveyPage from './pages/SurveyPage/SurveyPage';
 
 function App() {
-  const questions = questionsData;
-
-  const steps = 0;
-
-  const [answers, setAnswers] = useState([]);
-
   return (
-    <div className="App">
-      <ProgressIndicator />
-      <QuestionBox
-        question={questions[steps]}
-        steps={steps}
-        questionsLength={questions.length}
-        answer={answers[steps]}
-        setAnswer={(newAnswer) => {
-          setAnswers((answers) => {
-            const copyAnswers = [...answers];
-            copyAnswers[steps] = newAnswer;
-            return copyAnswers;
-          });
-        }}
-      />
+    <div className={'App'}>
+      <Routes>
+        <Route path={'/survey/:surveyId'} element={<SurveyPage />}>
+          <Route path={':step'} element={<SurveyPage />} />
+        </Route>
+        <Route path={'/done'} element={<CompletionPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/SurveyPage/SurveyPage.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { useParams } from 'react-router';
+
+import ProgressIndicator from '../../components/ProgressIndicator/ProgressIndicator';
+import QuestionBox from '../../components/QuestionBox/QuestionBox';
+import questionsData from '../../data/questionsData';
+
+function SurveyPage() {
+  const questions = questionsData;
+
+  const [answers, setAnswers] = useState([]);
+
+  const params = useParams();
+
+  const steps = parseInt(params.step);
+
+  return (
+    <>
+      <ProgressIndicator />
+      <QuestionBox
+        question={questions[steps]}
+        steps={steps}
+        questionsLength={questions.length}
+        answer={answers[steps]}
+        setAnswer={(newAnswer) => {
+          setAnswers((answers) => {
+            const copyAnswers = [...answers];
+            copyAnswers[steps] = newAnswer;
+            return copyAnswers;
+          });
+        }}
+      />
+    </>
+  );
+}
+
+export default SurveyPage;
