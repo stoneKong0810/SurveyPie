82d29761f200ae037529a8f3ade548d7f00fa8c8
stoneKong0810 <stonekong2115@gmail.com>
2025-03-16 19:30:45 +0900

    Docs: 데이터 추가

--- src/data/questionsData.js
+++ src/data/questionsData.js
@@ -4,14 +4,27 @@ const questionsData = [
     description: '1번 설명입니다.',
     type: 'text',
     required: false,
-    options: {},
+    options: {
+      placeholder: '10자 이내 단답',
+    },
   },
   {
     title: '2번 질문 입니다.',
     description: '2번 설명입니다.',
-    type: 'text',
+    type: 'textarea',
     required: true,
-    options: {},
+    options: {
+      placeholder: '400자 이내 답',
+    },
+  },
+  {
+    title: '3번 질문 입니다.',
+    description: '3번 설명입니다.',
+    type: 'select',
+    required: true,
+    options: {
+      items: ['답변1', '답변2', '답변3', '답변4', '답변5'],
+    },
   },
 ];
 
